1388a0b171ca2fc1e2927530ce78cd6983c588bf
antihumanohm <unloveable10753@gmail.com>
2021-02-26 13:57:53 +0700

    Excrcise4_Sarun_S

--- Excrcise4_Sarun_S.py
+++ Excrcise4_Sarun_S.py
@@ -3,7 +3,7 @@ basic_bus = 78
 basic_com = 80
 basic_pro = 89
 
-print("---You core---")
+print("---You Score---")
 print("Foundation English : คะแนนที่ได้",basic_eng)
 print("General Business : คะแนนที่ได้",basic_bus)
 print("Introduction to Computer Systems : คะแนนที่ได้",basic_com)
