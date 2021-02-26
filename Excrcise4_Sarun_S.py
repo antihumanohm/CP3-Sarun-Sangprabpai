56c3b3dc16b59d4a14aa06891acd5256cd0df6d2
antihumanohm <unloveable10753@gmail.com>
2021-02-26 14:02:35 +0700

    Excrcise4_Sarun_S

--- Excrcise4_Sarun_S.py
+++ Excrcise4_Sarun_S.py
@@ -1,7 +1,7 @@
-basic_eng = 98
-basic_bus = 78
-basic_com = 80
-basic_pro = 89
+basic_eng = 98.0
+basic_bus = 78.0
+basic_com = 80.0
+basic_pro = 89.0
 
 print("---You Score---")
 print("Foundation English : คะแนนที่ได้",basic_eng)
