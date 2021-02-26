22555ae73a178f69ec8c419b5c865a7d3dfdb7da
antihumanohm <unloveable10753@gmail.com>
2021-02-26 13:56:04 +0700

    Add files via upload

---
+++ Excrcise4_Sarun_S.py
@@ -0,0 +1,10 @@
+basic_eng = 98
+basic_bus = 78
+basic_com = 80
+basic_pro = 89
+
+print("---You core---")
+print("Foundation English : คะแนนที่ได้",basic_eng)
+print("General Business : คะแนนที่ได้",basic_bus)
+print("Introduction to Computer Systems : คะแนนที่ได้",basic_com)
+print("Computer Programming : คะแนนที่ได้",basic_pro)
