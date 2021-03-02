2c2d14141b1a909d0c3209ebeaddf47f9bc5d5e1
antihumanohm <unloveable10753@gmail.com>
2021-03-02 14:00:27 +0700

    Lecture50_Sarun_S

---
+++ Lecture50_Sarun_S.py
@@ -0,0 +1,13 @@
+def plus(x,y):
+    print("ผลบวก = ",x+y)
+def minus(x,y):
+    print("ผลลบ = ",x-y)
+def multiplied(x,y):
+    print("ผลคูณ = ",x*y)
+def divide(x,y):
+    print("ผลหาร = ",x/y)
+
+plus(20,5)
+minus(20,5)
+multiplied(20,5)
+divide(20,5)
