af1e61220d20b4873b5d078cb599b24ea37e665b
antihumanohm <unloveable10753@gmail.com>
2021-03-04 14:57:20 +0700

    Lecture72_Sarun_S #ส่งผิดข้อครับ

--- Lecture73_Sarun_S.py
+++ Lecture73_Sarun_S.py
@@ -1,11 +1,14 @@
 menulist = []
+
+
 def showbill():
     print("----My Food----")
     totalprice = 0
     for number in range(len(menulist)):
-        totalprice+= int(menulist[number][1])
-        print(menulist[number][0],"ราคา",menulist[number][1])
-    print("รวมราคาสินค้า : ",totalprice)
+        totalprice += int(menulist[number][1])
+        print(menulist[number][0], "ราคา", menulist[number][1])
+    print("รวมราคาสินค้า : ", totalprice)
+
 
 while True:
     menuname = input("กรอกรายชื่อสินค้า : ")
@@ -13,6 +16,6 @@ while True:
         break
     else:
         menuprice = int(input("กรอกราคาสินค้า : "))
-        menulist.append([menuname,menuprice])
+        menulist.append([menuname, menuprice])
 
 (showbill())
